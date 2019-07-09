85ff8f3c95361031647edd30efd44b9e587b7e92
chandana <jampala.c@gmail.com>
2019-07-10 04:30:29 +0530

    service file

--- src/app/registration.Service.ts
+++ src/app/registration.Service.ts
@@ -18,12 +18,11 @@ constructor(private http: HttpClient){}
 
 saveToDb(toDoList){
     console.log("in save")
- return this.http.post('http://localhost:8084/regForm/addUsers', toDoList,httpOptions).subscribe(res=> console.log(res));
+ return this.http.post('http://localhost:8084/regForm/addUsers', toDoList,httpOptions).subscribe(res=> 
+ 
+ console.log(res));
+ 
 }
 
-// getDb() {
-// const headers = new Headers();
-// headers.append('Access-Control-Allow-Origin', '*');
-// return this.http.get('https://ng-todo-d052e.firebaseio.com/list.json', {headers: headers});
-// }
+
 }
